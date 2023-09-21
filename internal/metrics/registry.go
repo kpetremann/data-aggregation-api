4509c9e5a6ba71a83c21223d7a0811176cac8685
Kevin Petremann <k.petremann@criteo.com>
2023-09-21 09:29:44 +0200

    feat(metrics): add build duration metrics

--- internal/metrics/registry.go
+++ internal/metrics/registry.go
@@ -11,6 +11,11 @@ type Registry struct {
 	BuiltDevicesNumber *prometheus.GaugeVec
 	lastBuildStatus    *prometheus.GaugeVec
 	buildTotal         *prometheus.CounterVec
+
+	buildTotalDuration        prometheus.Gauge
+	buildDataFetchingDuration prometheus.Gauge
+	buildPrecomputeDuration   prometheus.Gauge
+	buildComputeDuration      prometheus.Gauge
 }
 
 func NewRegistry() Registry {
@@ -40,13 +45,36 @@ func NewRegistry() Registry {
 			},
 			[]string{},
 		),
-
 		buildTotal: promauto.NewCounterVec(
 			prometheus.CounterOpts{
 				Name: "completed_build_total",
 				Help: "Total number of completed build",
 			},
 			[]string{"success"},
+		),
+		buildTotalDuration: promauto.NewGauge(
+			prometheus.GaugeOpts{
+				Name: "build_total_duration_seconds",
+				Help: "Total duration of the build",
+			},
+		),
+		buildDataFetchingDuration: promauto.NewGauge(
+			prometheus.GaugeOpts{
+				Name: "build_data_fetching_duration_seconds",
+				Help: "Duration of the data fetching step",
+			},
+		),
+		buildPrecomputeDuration: promauto.NewGauge(
+			prometheus.GaugeOpts{
+				Name: "build_precompute_duration_seconds",
+				Help: "Duration of the precompute step",
+			},
+		),
+		buildComputeDuration: promauto.NewGauge(
+			prometheus.GaugeOpts{
+				Name: "build_compute_duration_seconds",
+				Help: "Duration of the compute step",
+			},
 		),
 	}
 }
@@ -72,4 +100,24 @@ func (r *Registry) BuildFailed() {
 // SetBuiltDevices updates the `built_devices` gauge.
 func (r *Registry) SetBuiltDevices(count uint32) {
 	r.BuiltDevicesNumber.WithLabelValues().Set(float64(count))
+}
+
+// SetBuildTotalDuration updates the `build_total_duration_seconds` gauge.
+func (r *Registry) SetBuildTotalDuration(duration float64) {
+	r.buildTotalDuration.Set(duration)
+}
+
+// SetBuildDataFetchingDuration updates the `build_data_fetching_duration_seconds` gauge.
+func (r *Registry) SetBuildDataFetchingDuration(duration float64) {
+	r.buildDataFetchingDuration.Set(duration)
+}
+
+// SetBuildPrecomputeDuration updates the `build_precompute_duration_seconds` gauge.
+func (r *Registry) SetBuildPrecomputeDuration(duration float64) {
+	r.buildPrecomputeDuration.Set(duration)
+}
+
+// SetBuildComputeDuration updates the `build_compute_duration_seconds` gauge.
+func (r *Registry) SetBuildComputeDuration(duration float64) {
+	r.buildComputeDuration.Set(duration)
 }
